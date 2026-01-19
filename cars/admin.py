b1b323bd3265446afa2a5f5d7c3c7221ef249c81
Kauã Amado <kauaamado5@gmail.com>
2026-01-19 20:56:19 -0300

    fix: corrige exibição das brands (marcas)

--- cars/admin.py
+++ cars/admin.py
@@ -4,6 +4,7 @@ from cars.models import Car, Brand
 class BrandAdmin(admin.ModelAdmin):
     list_display = ('name',)
     search_fields = ('name',)
+
 class CarAdmin(admin.ModelAdmin):
     list_display = ('model', 'brand', 'factory_year', 'model_year', 'value')
     
--- cars/models.py
+++ cars/models.py
@@ -5,6 +5,9 @@ from django.db import models
 class Brand(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.name
 
 class Car(models.Model):
     id = models.AutoField(primary_key=True)
