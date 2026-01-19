0d853a0e86f7998f4e69b8fcca89f02a45417666
Kauã Amado <kauaamado5@gmail.com>
2026-01-19 19:02:35 -0300

    feat: cria modelo/tabela Car na Database

--- cars/models.py
+++ cars/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Car(models.Model):
+    id = models.AutoField(primary_key=True)
+    model = models.CharField(max_length=200, blank=False, null=False)
+    brand = models.CharField(max_length=200, blank=False, null=False)
+    factory_year = models.IntegerField(blank=True, null=False)
+    model_year = models.IntegerField(blank=True, null=False)
+    value = models.FloatField(blank=True, null=False)
