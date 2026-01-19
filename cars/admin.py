337ec427178038563ca539066b2ab0c6270d266f
Kauã Amado <kauaamado5@gmail.com>
2026-01-19 20:42:21 -0300

    feat: registra o menu Brands no painel admin

--- cars/admin.py
+++ cars/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from cars.models import Car
+from cars.models import Car, Brand
 
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
 class CarAdmin(admin.ModelAdmin):
     list_display = ('model', 'brand', 'factory_year', 'model_year', 'value')
     
@@ -8,4 +11,5 @@ class CarAdmin(admin.ModelAdmin):
     search_fields = ('model', 'brand')
 
 # Registra no site admin
+admin.site.register(Brand, BrandAdmin)
 admin.site.register(Car, CarAdmin)
