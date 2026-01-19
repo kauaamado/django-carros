478b0a575d4c4caddca4692260551d1b40791e90
Kauã Amado <kauaamado5@gmail.com>
2026-01-19 20:37:37 -0300

    feat: torna o campo brand (marcas) em uma lista selecionável.
    - Para adicionar uma marca, agora o usuário deve apenas selecionar a marca, não digitar o nome.

--- cars/models.py
+++ cars/models.py
@@ -2,10 +2,14 @@ from django.db import models
 
 # Create your models here.
 
+class Brand(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+
 class Car(models.Model):
     id = models.AutoField(primary_key=True)
     model = models.CharField(max_length=200, blank=False, null=False)
-    brand = models.CharField(max_length=200, blank=False, null=False)
+    brand = models.ForeignKey(Brand, on_delete=models.PROTECT, related_name='car_brand')
     factory_year = models.IntegerField(blank=True, null=False)
     model_year = models.IntegerField(blank=True, null=False)
     value = models.FloatField(blank=True, null=False)
