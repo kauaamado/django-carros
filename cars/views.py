f1c33442f6e3fba51dbb20fae4a9e46b2b2f9f73
Kauã Amado <kauaamado5@gmail.com>
2026-01-27 16:20:00 -0300

    feat: adiciona pesquisa na página /cars

--- cars/views.py
+++ cars/views.py
@@ -5,6 +5,10 @@ from cars.models import Car
 
 def cars_view(request):
     cars = Car.objects.all()
+    search = request.GET.get('search')
+
+    if search:
+        cars = Car.objects.filter(model__icontains=search)
 
     return render(
         request,
