b5e1f06f9eee3e16bcb62f89b703a9399934db0c
Kauã Amado <kauaamado5@gmail.com>
2026-01-27 16:38:14 -0300

    feat: adiciona template com CSS para /cars

--- cars/views.py
+++ cars/views.py
@@ -4,11 +4,11 @@ from cars.models import Car
 # ~~~~{ Views da página /cars }~~~~ #
 
 def cars_view(request):
-    cars = Car.objects.all()
+    cars = Car.objects.all().order_by('brand', 'model')
     search = request.GET.get('search')
 
     if search:
-        cars = Car.objects.filter(model__icontains=search)
+        cars = Car.objects.filter(model__icontains=search).order_by('model')
 
     return render(
         request,
